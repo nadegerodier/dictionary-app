5dadc221c3302d8823a5a7dbd8e44d2917299929
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2023-05-29 15:24:26 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Dictionary</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Dictionary
+        <a href="#" className="btn btn-primary">
+          Hello
+        </a>
+      </div>
+    </div>
+  );
 }
