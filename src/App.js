02911b394b3f46c447e211b03144a62bba8bdf29
NadegeRodier <129798006+NadegeRodier@users.noreply.github.com>
2023-05-30 15:35:26 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <footer className="mt-5">
+          <a
+            href="https://github.com/nadegerodier/dictionary-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://incomparable-starburst-3e5eae.netlify.app"
+            target="_blank"
+          >
+            Nadege Rodier
+          </a>
+        </footer>
       </div>
     </div>
   );
